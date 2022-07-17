6f0bc7208c5c46820eb53d119fea1ad64bccde7e
Yoonjiwon <k112751@koreainvestment.com>
2022-07-18 07:22:28 +0900

    Add a Test button
    * Added a button object
    * Added a setupViews function
    * set a layout constraints for button

--- PopupViewWindow/ViewController.swift
+++ PopupViewWindow/ViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    private lazy var testButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TEST", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.backgroundColor = .green
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+}
+
+private extension ViewController {
+    func setupViews() {
+        [
+            testButton
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        testButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
 }
 
